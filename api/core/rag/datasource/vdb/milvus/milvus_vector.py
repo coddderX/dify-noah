fad6c918228ff29ed3f18474171a4edb98deac17
xianghui <xianghui@corp.netease.com>
2025-03-03 18:07:34 +0800

    尝试使用最新的代码，但是关闭milvus的混合检索

--- api/core/rag/datasource/vdb/milvus/milvus_vector.py
+++ api/core/rag/datasource/vdb/milvus/milvus_vector.py
@@ -72,7 +72,8 @@ class MilvusVector(BaseVector):
         self._client = self._init_client(config)
         self._consistency_level = "Session"  # Consistency level for Milvus operations
         self._fields: list[str] = []  # List of fields in the collection
-        self._hybrid_search_enabled = self._check_hybrid_search_support()  # Check if hybrid search is supported
+        #self._hybrid_search_enabled = self._check_hybrid_search_support()  # Check if hybrid search is supported
+        self._hybrid_search_enabled = False # 固定为false
 
     def _check_hybrid_search_support(self) -> bool:
         """
